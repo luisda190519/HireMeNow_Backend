756d5f7d728f9ed85a5fbfb3af00d0bb6152561a
luisda190519 <licerol@uninorte.edu.co>
2023-04-16 14:20:57 -0500

    Authentication

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const cors = require("cors");
+const bcrypt = require('bcrypt');
+const User = require("./models/userModel");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
@@ -15,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({ credentials: true, origin: true }));
 
 //Base de datos
 mongoose
@@ -37,8 +41,49 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(
+    new LocalStrategy(
+        { usernameField: "email" },
+        async (email, password, done) => {
+            try {
+                const user = await User.findOne({ email: email });
+                if (!user) {
+                    return done(null, false, {
+                        message: "Incorrect email or password",
+                    });
+                }
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return done(null, false, {
+                        message: "Incorrect email or password",
+                    });
+                }
+                return done(null, user);
+            } catch (error) {
+                console.log(error);
+                return done(error);
+            }
+        }
+    )
+);
+
+// Passport Session Serialization
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        console.log(error);
+        done(error);
+    }
+});
+
 //Rutas
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
-router.get("/login", (req, res) => {
-    res.send("hola");
+router.post("/signup", async (req, res) => {
+    try {
+        const { email, password, firstName, lastName, cellphone } = req.body;
+        const userExists = await User.findOne({ email: email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName,
+            cellphone: cellphone,
+        });
+        await user.save();
+        req.login(user, (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Server Error" });
+            }
+            return res
+                .status(201)
+                .json({ message: "User created successfully" });
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+    return res.status(200).json({ message: "Login successful" });
+});
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect("/login");
 });
 
 module.exports = router;
