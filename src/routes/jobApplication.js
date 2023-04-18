2132b0601000e267a43a464c0a723f91abe24367
luisda190519 <licerol@uninorte.edu.co>
2023-04-18 08:58:54 -0500

    Update jobApplication.js

--- src/routes/jobApplication.js
+++ src/routes/jobApplication.js
@@ -25,7 +25,7 @@ router.post("/postJob", async (req, res) => {
             requirements,
             tags,
             image,
-            owner
+            owner,
         } = req.body;
 
         const job = new Job({
@@ -38,7 +38,7 @@ router.post("/postJob", async (req, res) => {
             requirements,
             tags,
             image,
-            owner
+            owner,
         });
 
         const savedJob = await job.save();
@@ -74,13 +74,55 @@ router.get("/:location", async (req, res) => {
     }
 });
 
-router.get("/:title/:company/:location", async (req, res) => {
-    const { title, location } = req.query;
-    const jobs = await Job.find({
-        title: new RegExp(title, "i"),
-        location: new RegExp(location, "i"),
-    });
-    res.json(jobs);
+router.get(
+    "/title/:title/company/:company/location/:location",
+    async (req, res) => {
+        const { title, location } = req.query;
+        const jobs = await Job.find({
+            title: new RegExp(title, "i"),
+            location: new RegExp(location, "i"),
+        });
+        res.json(jobs);
+    }
+);
+
+router.get("/title/:title/place/:location", async (req, res) => {
+    const { title, location } = req.params;
+
+    try {
+        const jobs = await Job.find({
+            $or: [
+                { title: { $regex: new RegExp(title, "i") } },
+                { location: { $regex: new RegExp(location, "i") } },
+            ],
+        });
+        res.json(jobs);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.get("/title/:title", async (req, res) => {
+    const { title } = req.params;
+
+    try {
+        const jobs = await Job.find({
+            title: { $regex: new RegExp(title, "i") },
+        });
+
+        res.json(jobs);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.get("/randomJobs", async (req, res) => {
+    try {
+        const jobs = await Job.aggregate([{ $sample: { size: 7 } }]);
+        res.json(jobs);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 });
 
 module.exports = router;
