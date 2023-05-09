fcb59bfc4080c6316c9cf2b29e724da88f97ed9a
licerol@uninorte.edu.co <licerol@uninorte.edu.co>
2023-05-09 09:40:22 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5OTk0NA/spectral.js?branch_slug=main&token=0bdab8cb-a27e-4644-b502-5869374cbf06";
+export default { extends: ruleset };
